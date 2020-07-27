d571980ece1e7745bf269575d4f876ac395b6d88
Builder Daemon <m4gw4s+buildmachine@gmail.com>
2020-07-27 09:15:01 +0000

    fixup

--- src/rules.js
+++ src/rules.js
@@ -2,12 +2,10 @@ const noCommentRule = require("./noCommentRule")
 
 console.log(noCommentRule)
 module.exports = {
-  rules : {
     'no-comment': {
       meta: {
         fixable: "code",
       },
       create: noCommentRule()
     }
-  }
 }

1f3e358745d34c282a95ac49d43a9836a16813a5
Mayuresh Kutte <57801513+Mayuresh94@users.noreply.github.com>
2020-08-09 13:18:02 +0530

    eslint/rule_1 : Entity

--- rules.js
+++ rules.js
@@ -1,5 +1,8 @@
 module.exports = {
     "import/no-default-export": 2,
     "kodekonveyor/no-comment": 2,
-    "promise/prefer-await-to-then": "error"
+    "promise/prefer-await-to-then": "error",
+    "filenames/match-regex": [2, "(^[a-z]+[A-Z]+[a-z]+[.](?:tsx)$).*", true],
+    "filenames/match-exported": 2,
+    "no-restricted-syntax": "error"
   }

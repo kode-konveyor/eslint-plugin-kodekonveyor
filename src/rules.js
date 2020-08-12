f758ab15b4fead4b7698269c924b7652fed97960
Tamas Szirbucz <szirbucz@kodekonveyor.com>
2020-08-12 10:56:10 +0200

    Config npm packaging

--- src/rules.js
+++ src/rules.js
@@ -3,6 +3,29 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-const rules = exports.rules = {
-  'no-comment': require('./noCommentRule')
+
+module.exports = {
+  rules: {
+    'no-comment': require('./noCommentRule')
+  },
+  configs: {
+    recommended: {
+	    plugins: [
+		    "@kode-konveyor"
+	    ],
+	    extends: [
+    		'eslint:recommended',
+    		'plugin:@typescript-eslint/recommended',
+    		"plugin:react/recommended",
+  	    ],
+	    env: {
+		    "browser": true,
+		    "jest": true,
+		    "es6": true,
+	    },
+  	    rules: {
+    	    	'@kode-konveyor/no-comment': 2
+  	    },
+    }
+  }
 }

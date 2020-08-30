1cd1fd9867b82b9a7f27ca6cc1c99dfe7b1be447
Mayuresh Kutte <57801513+Mayuresh94@users.noreply.github.com>
2020-08-30 14:14:20 +0530

    esint/rule_4 : no literals (#5)

---
+++ test/literals.test.tsx
@@ -0,0 +1,29 @@
+import assert from "assert";
+import {RuleTester} from "eslint"
+const noLiteralsRule = require("rules").rules["no-literals"];
+
+console.log(noLiteralsRule)
+const ruleTester = new RuleTester();
+
+const greet = 'Hello'; 
+const input = console.log(greet);
+const a = it("empty input, empty output", () => {});
+const b = describe("MakeScheduleSelectionsService", () => {});
+
+ruleTester.run("no-literal", noLiteralsRule, {
+  valid: [	"input",
+			"console.log(1)",
+			"console.log(0)",
+			"a",
+			"b"		
+  ],
+
+  invalid: [
+    {
+      code: "console.log('Hello')",
+      errors: [
+        { message: "There should be no literals, except 0 and 1." },
+       ],
+    },
+  ]
+});
